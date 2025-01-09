47d001d76ad08f349994fce9c4f267b9b71bd24f
Dhairya3391 <dhairyaadroja3391@gmail.com>
2025-01-09 13:03:30 +0530

    lab 7

--- playground.py
+++ playground.py
@@ -20,11 +20,11 @@
 # for i in range(0,6):
 #     print("*"*i*2)
 
-list1 = input("Enter the first list of elements separated by spaces: ").split()
-list2 = input("Enter the second list of elements separated by spaces: ").split()
-common_elements = list(set(list1) & set(list2))
+# list1 = input("Enter the first list of elements separated by spaces: ").split()
+# list2 = input("Enter the second list of elements separated by spaces: ").split()
+# common_elements = list(set(list1) & set(list2))
 
-print("Common elements:", common_elements)
+# print("Common elements:", common_elements)
 
 # def pattern(n):
 #     length =2*n-1
@@ -32,3 +32,37 @@ print("Common elements:", common_elements)
 #         print(" ".join(str(n-min(i,j,length-1-i,length-1-j)) for j in range(length)))
 
 # pattern(int(input("enter number : ")))
+
+# def isPrime(n):
+#     flag = True
+#     for i in range(2,n):
+#         if (n%i==0):
+#             flag = False
+#     return flag
+
+# def primeRange(m,n):
+#     lst = []
+#     for i in range(m,n):
+#         if(isPrime(i)):
+#             lst.append(i)
+#     return lst
+
+# # print(primeRange(10,20))
+
+# def foo(bar: float) -> float:
+#     if not isinstance(bar, float):
+#         raise TypeError("learned something i guess") #that python can not be use as typed language
+#     return bar
+# print(foo(10))
+
+def createDict():
+    dict1 = {}
+    n = int(input("Enter the number of elements: "))
+    for i in range(n):
+        key = input("Enter the key: ")
+        value = input("Enter the value: ")
+        dict1[key] = value
+    return dict1
+
+d = createDict()
+print(d)
